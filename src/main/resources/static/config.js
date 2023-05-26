d99b907a1ceed63a09c921143e03b796fc05cd99
hwan1625 <seung1625@gmail.com>
2023-05-26 20:46:14 +0900

    config 수정

--- src/main/resources/static/config.js
+++ src/main/resources/static/config.js
@@ -1,2 +1,2 @@
-export default {ip: '52.78.223.106:8083',};
-// export default {ip: 'everyparking.co.kr'};
+// export default {ip: '52.78.223.106:8083',};
+export default {ip: 'everyparking.co.kr'};
